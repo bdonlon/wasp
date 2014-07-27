efe8b886921455a07f12b4277b6843d67fc03fcb
bdonlon <bdonlon@Ben-Donlons-MacBook-Pro.local>
2014-07-27 22:22:01 +0100

    Sound on hit for wasps
    A (placeholder) sound plays when wasps are struck. Also updating
    .gitignore

---
+++ Wasp1/Assets/Scripts/playSound.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playSound : MonoBehaviour {
+
+	public AudioClip sound;
+	public AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void play(){
+		if (sound)
+			audioSource.clip = sound;
+		audioSource.Play();
+	}
+}
--- Wasp1/Assets/Scripts/waspSwatTrigger.cs
+++ Wasp1/Assets/Scripts/waspSwatTrigger.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class waspSwatTrigger : MonoBehaviour {
 
 	public GameObject waspParent;
+	public GameObject hitSound;
+	public GameObject killSound;
 
 	void OnTriggerEnter2D(Collider2D col)
 	{
@@ -13,6 +15,7 @@ public class waspSwatTrigger : MonoBehaviour {
 		}else if(col.gameObject.tag == "swatter"){
 			//subtract health
 			waspParent.GetComponent<waspBehaviour>().takeDamage(50);
+			hitSound.GetComponent<playSound>().play();
 			//then if health <=0
 				//gameObject.SendMessageUpwards("kill");
 				//Destroy(this.gameObject,0);
