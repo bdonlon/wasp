33510b519022cc103a1002820cbe86f25f21d9be
bdonlon <bndonlon@gmail.com>
2014-09-30 23:30:39 +0100

    Improving knockback wasp receives from swatter
    Force is applied as a vector from player position to wasp position so
    knockback is always diametrically away from player.
    
    Previously was just inverting wasp current travel direction and applying
    a larger force.

--- Wasp1/Assets/Scripts/waspSwatTrigger.cs
+++ Wasp1/Assets/Scripts/waspSwatTrigger.cs
@@ -7,16 +7,11 @@ public class waspSwatTrigger : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D col)
 	{
-
 		if(waspParent.GetComponent<waspBehaviour>().isDead()){
 			//do nothing
 		}else if(col.gameObject.tag == "swatter"){
 			//subtract health
 			waspParent.GetComponent<waspBehaviour>().takeDamage(50);
-			//hitSound.GetComponent<playSound>().play();
-			//then if health <=0
-				//gameObject.SendMessageUpwards("kill");
-				//Destroy(this.gameObject,0);
 		}
 	}
 }
