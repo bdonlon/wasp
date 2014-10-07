1d49c8128c9dd7589d5480957f6ef5f8178fea8f
bdonlon <bndonlon@gmail.com>
2014-10-07 23:32:25 +0100

    Grass animation
    Grass rustles when run over

---
+++ Wasp1/Assets/script_grass_rustle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class script_grass_rustle : MonoBehaviour {
+
+	Animator anim;
+
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+
+		if(col.gameObject.tag=="player_feet"){
+			anim.SetTrigger("rustle");
+		}
+	}
+}
