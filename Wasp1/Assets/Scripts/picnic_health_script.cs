8478f6896d5b4935a5e4621ca7a3693e3b933339
bdonlon <bndonlon@gmail.com>
2014-12-18 23:01:00 +0000

    Picnic healthbar

--- Wasp1/Assets/Scripts/picnic_health_script.cs
+++ Wasp1/Assets/Scripts/picnic_health_script.cs
@@ -9,20 +9,33 @@ public class picnic_health_script : MonoBehaviour {
 	public GameObject spouse;
 	public GameObject food;
 	public GameObject _GM;
+	public GameObject hb;
+	public float healthBarWidth,healthBarSize;
+
+	public float timeEaten;
+	public float elapsedTime;
 
 	// Use this for initialization
 	void Start () {
+		timeEaten =  Time.time-100;
+		healthBarSize=2;
 		picnicHealthCurrent = 200;
 		picnicHealthTotal = picnicHealthCurrent;
 	}
 	
 	// Update is called once per frame
-//	void Update () {
-//		picnicPercentage = 100 - picnicHealth
-//	}
+	void Update () {
+		drawHealthBar();
+
+		float healthPercent = getPercentage();
+		healthBarWidth = healthPercent*healthBarSize;
+		hb.transform.Find("red").guiTexture.pixelInset=new Rect(0,0,100*healthBarSize,5);
+		hb.transform.Find("green").guiTexture.pixelInset=new Rect(0,0,healthBarWidth,5);
+	}
 
 	public void damagePicnicIntegrity(int damage){
 		if((picnicHealthCurrent-damage)>=0){
+			timeEaten =  Time.time;
 			picnicHealthCurrent = picnicHealthCurrent - damage;
 
 			if(picnicHealthCurrent <= 0){
@@ -38,5 +51,14 @@ public class picnic_health_script : MonoBehaviour {
 //		Debug.Log (picnicHealthTotal);
 //		Debug.Log (picnicHealthCurrent);
 		return value;
+	}
+
+	void drawHealthBar(){
+		elapsedTime =  Time.time - timeEaten;
+		if(elapsedTime<1.5f){
+			hb.active=true;
+		}else{
+			hb.active=false;
+		}
 	}
 }
--- Wasp1/Assets/script_controls_graphic.cs
+++ Wasp1/Assets/script_controls_graphic.cs
@@ -5,15 +5,31 @@ public class script_controls_graphic : MonoBehaviour {
 
 	public GameObject graphic_move;
 	public GameObject graphic_swing;
+	SpriteRenderer rend1;
+	SpriteRenderer rend2;
 
 	// Use this for initialization
 	void Start () {
-		StartCoroutine(moveGraphics());
+		//StartCoroutine(moveGraphics());
+		rend1=graphic_move.GetComponent<SpriteRenderer>();
+		rend2=graphic_swing.GetComponent<SpriteRenderer>();
+		StartCoroutine(fadeOut());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public IEnumerator fadeOut()
+	{
+		for (float i = 255; i >= 0; i--) {
+			rend1.color = new Color(rend1.color.r, rend1.color.g, rend1.color.b, i/255*3);
+			rend2.color = new Color(rend2.color.r, rend2.color.g, rend2.color.b, i/255*3);
+			yield return new WaitForSeconds(0.0f);
+		}
+
+		Destroy(this.gameObject);
 	}
 
 	public IEnumerator moveGraphics(){
